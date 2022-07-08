73f3729b82b4c9d52a44fde1671dafbc8923436c
KUMA93 <103379701+KUMA93@users.noreply.github.com>
2022-07-08 22:41:47 +0900

    [Silver I] Title: 안전 영역, Time: 272 ms, Memory: 124208 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -8,10 +8,12 @@ maxH=-2147000000
 minH=2147000000
 for i in range(n):
     tmp=list(map(int,input().split()))
+    for j in range(n):
+        maxH=max(maxH, tmp[j])
     board.append(tmp)
 dx=[-1,0,1,0]
 dy=[0,-1,0,1]
-#dfs 코드
+#dfs 코드 - 재귀 한도 초과 떠서 한도 풀면 메모리 초과뜸 ㅠ bfs로 하는게 맞는듯
 # def DFS(x, y, h): #높이가 h이상인 지점 i,j와 이어져 있는 h이상인 지점들을 모두 탐색한다
 #     ch[x][y]=1
 #     for i in range(4):
@@ -39,7 +41,8 @@ dy=[0,-1,0,1]
 #bfs 코드
 dq=deque()
 res=0
-for h in range(0, 100):
+#계속 틀리길래 왜그런가 했더니 시작 범위를 1이 아닌 0으로 해야 맞는 처리가 됨.. 즉, 비가 안오는 경우를 고려해야함
+for h in range(0, 100): 
     cnt = 0
     ch = [[0] * n for _ in range(n)]
     for i in range(n):
