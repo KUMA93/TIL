262499b7dea05182b0b9cbe9dd99238b19a188a8
KUMA93 <103379701+KUMA93@users.noreply.github.com>
2022-07-09 15:14:56 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,17 +2,16 @@ import sys
 input=sys.stdin.readline
 
 n, k=map(int, input().split())
-
-# 람다 정렬로는 애초에 n이 500까지 가게 되면 500*500의 정보량을 정렬해야하니 복잡도가 높다. 다른 방식으로 풀어봐야 한다.
 score=[]
 for i in range(n):
     score.append(list(map(int, input().split())))
 score.sort(key=lambda x: (-x[1], -x[2], -x[3]))
 
 for i in range(n):
-    if score[i][0]==k:
-        idx=i
+    score[i].append(i+1)
+for i in range(1, n):
+    if score[i][1:4]==score[i-1][1:4]:
+        score[i][4]=score[i-1][4]
 for i in range(n):
-    if score[i][1:]==score[idx][1:]:
-        print(i+1)
-        break
+    if score[i][0]==k:
+        print(score[i][4])
