a44e9493efb6c2651ba714ff13d60712c0982fdb
KUMA93 <103379701+KUMA93@users.noreply.github.com>
2022-07-09 12:58:49 +0900

    [Silver IV] Title: 로프, Time: 164 ms, Memory: 118488 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+rope=[]
+for i in range(n):
+    rope.append(int(input()))
+rope.sort(reverse=True)
+maxW=0
+for i in range(n):
+    tmp=rope[i]*(i+1)
+    maxW=max(maxW, tmp)
+print(maxW)
