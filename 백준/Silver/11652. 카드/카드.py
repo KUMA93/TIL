d8468f43208b4ca6048d0058015a10b72dc91085
KUMA93 <103379701+KUMA93@users.noreply.github.com>
2022-07-16 17:21:35 +0900

    [Silver IV] Title: 카드, Time: 196 ms, Memory: 122808 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+numList=dict()
+for i in range(n):
+    tmp=int(input())
+    if tmp in numList:
+        numList[tmp]+=1
+    else:
+        numList[tmp]=1
+maxC=0
+for key, val in numList.items():
+    if val>maxC:
+        maxC=val
+        maxKey=key
+    elif val==maxC:
+        maxKey=min(maxKey, key)
+print(maxKey)
