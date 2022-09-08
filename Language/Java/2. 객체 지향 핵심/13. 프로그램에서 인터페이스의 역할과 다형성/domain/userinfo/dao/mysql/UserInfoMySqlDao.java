3aa6eb8b9a3ecd01b9198db18e032147bf6ce43f
KUMA93 <kyuh2002@gmail.com>
2022-09-08 09:01:24 +0900

    “0908”

---
+++ Language/Java/2. 객체 지향 핵심/13. 프로그램에서 인터페이스의 역할과 다형성/domain/userinfo/dao/mysql/UserInfoMySqlDao.java
@@ -0,0 +1,21 @@
+package ch13.domain.userinfo.dao.mysql;
+
+import ch13.domain.userinfo.UserInfo;
+import ch13.domain.userinfo.dao.UserInfoDao;
+
+public class UserInfoMySqlDao implements UserInfoDao {
+    @Override
+    public void insertUserInfo(UserInfo userInfo) {
+        System.out.println("Insert into MySQL DB userID = " + userInfo.getUserId());
+    }
+
+    @Override
+    public void updateUserInfo(UserInfo userInfo) {
+        System.out.println("Update into MySQL DB userID = " + userInfo.getUserId());
+    }
+
+    @Override
+    public void deleteUserInfo(UserInfo userInfo) {
+        System.out.println("Delete into MySQL DB userID = " + userInfo.getUserId());
+    }
+}
