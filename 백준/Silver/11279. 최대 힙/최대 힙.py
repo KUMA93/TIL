0684a0a59159f1ea836943861cff851eb0990d0e
KUMA93 <103379701+KUMA93@users.noreply.github.com>
2022-09-22 08:03:46 +0900

    [Silver II] Title: 최대 힙, Time: 188 ms, Memory: 36764 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input=sys.stdin.readline
+
+n=int(input())
+hq=[]
+
+for i in range(n):
+    tmp=int(input())
+    if tmp==0:
+        try:
+            print(-heapq.heappop(hq))
+        except:
+            print(0)
+    else:
+        heapq.heappush(hq, -tmp)
