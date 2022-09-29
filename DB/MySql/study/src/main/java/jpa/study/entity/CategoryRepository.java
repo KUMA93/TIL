51c161c370074af4f50f7199e69a00df1b443e8a
KUMA93 <kyuh2002@gmail.com>
2022-09-29 09:55:40 +0900

    “0929”

--- DB/MySql/study/src/main/java/jpa/study/entity/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package jpa.study.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- DB/MySql/study/src/main/java/jpa/study/entity/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package jpa.study.entity;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
