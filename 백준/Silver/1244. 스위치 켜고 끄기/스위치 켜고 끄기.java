34e505e1d15d29e266f554630938b74a03368307
KUMA93 <103379701+KUMA93@users.noreply.github.com>
2023-02-07 19:47:25 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 144 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -11,7 +11,7 @@ public class Main {
 	static int[] swt;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		//System.setIn(new FileInputStream("input.txt"));
+//		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
